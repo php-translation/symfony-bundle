c29061044a01416916dddf2ee8300726195b816d
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-12-26 22:41:29 +0100

    Added auto adder feature (#30)
    * Started on auto adder
    
    * Complete the auto adder feature.
    
    * Style fix
    
    * Require latest version of common

---
+++ EventListener/AutoAddMissingTranslations.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of the PHP Translation package.
+ *
+ * (c) PHP Translation team <tobias.nyholm@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Translation\Bundle\EventListener;
+
+use Translation\Common\Model\Message;
+use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\Translation\DataCollectorTranslator;
+use Translation\Bundle\Service\StorageService;
+
+/**
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+class AutoAddMissingTranslations
+{
+    /**
+     * @var DataCollectorTranslator
+     */
+    private $dataCollector;
+
+    /**
+     * @var StorageService
+     */
+    private $storage;
+
+    /**
+     * @param DataCollectorTranslator $translator
+     * @param StorageService          $storage
+     */
+    public function __construct(StorageService $storage, DataCollectorTranslator $translator = null)
+    {
+        $this->dataCollector = $translator;
+        $this->storage = $storage;
+    }
+
+    public function onTerminate(Event $event)
+    {
+        if ($this->dataCollector === null) {
+            return;
+        }
+
+        $messages = $this->dataCollector->getCollectedMessages();
+        foreach ($messages as $message) {
+            if ($message['state'] === DataCollectorTranslator::MESSAGE_MISSING) {
+                $m = new Message($message['id'], $message['domain'], $message['locale'], $message['translation']);
+                $this->storage->create($m);
+            }
+        }
+    }
+}
