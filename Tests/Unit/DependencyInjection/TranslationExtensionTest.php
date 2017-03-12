d2d1f56df1e6c4452587c32ccff801d9ae952788
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-03-12 08:44:13 +0100

    Added tests for TranslationExtension

---
+++ Tests/Unit/DependencyInjection/TranslationExtensionTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Translation\Bundle\Tests\Unit\Model;
+
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+use Symfony\Component\Translation\DataCollector\TranslationDataCollector;
+use Translation\Bundle\DependencyInjection\TranslationExtension;
+use Translation\Bundle\EventListener\AutoAddMissingTranslations;
+use Translation\Bundle\EventListener\EditInPlaceResponseListener;
+use Translation\Bundle\Translator\FallbackTranslator;
+
+class TranslationExtensionTest extends AbstractExtensionTestCase
+{
+    protected function getContainerExtensions()
+    {
+        $this->setParameter('kernel.default_locale', 'ar');
+        $this->setParameter('kernel.root_dir', __DIR__);
+
+        return [
+            new TranslationExtension(),
+        ];
+    }
+
+    public function testLocales()
+    {
+        $locales = ['fr', 'sv'];
+        $this->load(['locales' => $locales]);
+
+        $this->assertContainerBuilderHasParameter('php_translation.locales', $locales);
+        $this->assertContainerBuilderHasParameter('php_translation.default_locale', 'ar');
+    }
+
+    public function testDefaultLocales()
+    {
+        $this->load(['default_locale' => 'sv']);
+
+        $this->assertContainerBuilderHasParameter('php_translation.default_locale', 'sv');
+    }
+
+    public function testWebUiEnabled()
+    {
+        $this->load(['webui' => ['enabled' => true]]);
+
+        $this->assertContainerBuilderHasParameter('php_translation.webui.enabled', true);
+    }
+
+    public function testWebUiDisabled()
+    {
+        $this->load(['webui' => ['enabled' => false]]);
+
+        $this->assertContainerBuilderHasParameter('php_translation.webui.enabled', false);
+    }
+
+    public function testSymfonyProfilerEnabled()
+    {
+        $this->load(['symfony_profiler' => ['enabled' => true]]);
+
+        $this->assertContainerBuilderHasService('php_translation.data_collector', TranslationDataCollector::class);
+    }
+
+    public function testEditInPlaceEnabled()
+    {
+        $this->load(['edit_in_place' => ['enabled' => true]]);
+
+        $this->assertContainerBuilderHasService('php_translation.edit_in_place.response_listener', EditInPlaceResponseListener::class);
+    }
+
+    public function testAutoAddEnabled()
+    {
+        $this->load(['auto_add_missing_translations' => ['enabled' => true]]);
+
+        $this->assertContainerBuilderHasService('php_translator.auto_adder', AutoAddMissingTranslations::class);
+    }
+
+    public function testFallbackTranslationEnabled()
+    {
+        $this->load(['fallback_translation' => ['enabled' => true]]);
+
+        $this->assertContainerBuilderHasService('php_translator.fallback_translator', FallbackTranslator::class);
+    }
+}
