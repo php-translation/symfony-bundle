ed3cc7215aa09d769f305c7178fee2520e83cd9a
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-06-03 10:33:51 +0200

    cs

--- Twig/DummyTwigVisitor.php
+++ Twig/DummyTwigVisitor.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the PHP Translation package.
+ *
+ * (c) PHP Translation team <tobias.nyholm@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Translation\Bundle\Twig;
 
 /**
@@ -10,5 +19,4 @@ namespace Translation\Bundle\Twig;
  */
 class DummyTwigVisitor
 {
-
 }
