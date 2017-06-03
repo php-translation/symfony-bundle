aca8ff2c1dce1481a5c9303a65c082510d080029
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-06-03 10:29:56 +0200

    Make sure we do not bind to specific twig version before the TwigVisitorFactory has been executed

---
+++ Twig/DummyTwigVisitor.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Translation\Bundle\Twig;
+
+/**
+ * This dummy extractor is used to be compatible with both Twig1 and Twig2. It is only used in dependency injection
+ * container.
+ *
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+class DummyTwigVisitor
+{
+
+}
