bac9daa73c8bfe5669237539e6ee3c0525781795
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-06-15 17:50:00 +0200

    Do only not escape twit filter output when EditInPlace is false
    This will fix #100

--- Twig/TranslationExtension.php
+++ Twig/TranslationExtension.php
@@ -11,6 +11,10 @@
 
 namespace Translation\Bundle\Twig;
 
+use Symfony\Component\HttpFoundation\RequestStack;
+use Translation\Bundle\EditInPlace\ActivatorInterface;
+use Twig\Environment;
+
 /**
  * Override the `trans` functions `is_safe` option to allow HTML output from the
  * translator. This extension is used by for the EditInPlace feature.
@@ -20,14 +24,72 @@ namespace Translation\Bundle\Twig;
 class TranslationExtension extends \Symfony\Bridge\Twig\Extension\TranslationExtension
 {
     /**
+     * @var ActivatorInterface
+     */
+    private $activator;
+
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
+
+    /**
      * {@inheritdoc}
      */
     public function getFilters()
     {
         return [
-            new \Twig_SimpleFilter('trans', [$this, 'trans'], ['is_safe' => ['html']]),
-            new \Twig_SimpleFilter('transchoice', [$this, 'transchoice'], ['is_safe' => ['html']]),
+            new \Twig_SimpleFilter('trans', [$this, 'transAutoEscape'], ['is_safe' => ['html'], 'needs_environment' => true]),
+            new \Twig_SimpleFilter('transchoice', [$this, 'transchoiceAutoEscape'], ['is_safe' => ['html'], 'needs_environment' => true]),
         ];
+    }
+
+    /**
+     * Escape output if the EditInPlace is disabled.
+     *
+     * @return bool
+     */
+    private function escapeOutput()
+    {
+        return !$this->activator->checkRequest($this->requestStack->getMasterRequest());
+    }
+
+    public function transAutoEscape(Environment $env, $message, array $arguments = array(), $domain = null, $locale = null)
+    {
+        $value = $this->trans($message, $arguments, $domain, $locale);
+
+        if ($this->escapeOutput()) {
+            return  twig_escape_filter($env, $value, 'html', null, true);
+        }
+
+        return $value;
+    }
+
+    public function transchoiceAutoEscape(Environment $env, $message, $count, array $arguments = array(), $domain = null, $locale = null)
+    {
+        $value = $this->transchoice($message, $count, $arguments, $domain, $locale);
+
+        if ($this->escapeOutput()) {
+            return  twig_escape_filter($env, $value, 'html', null, true);
+        }
+
+        return $value;
+    }
+
+    /**
+     * @param ActivatorInterface $activator
+     */
+    public function setActivator(ActivatorInterface $activator)
+    {
+        $this->activator = $activator;
+    }
+
+    /**
+     * @param RequestStack $requestStack
+     */
+    public function setRequestStack(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
     }
 
     /**
