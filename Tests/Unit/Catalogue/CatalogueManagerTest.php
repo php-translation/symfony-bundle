9215c9f4ac9973d8e9b95c4ff6f9bf93ff042a12
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-08-19 21:50:28 +0200

    cs

--- Tests/Unit/Catalogue/CatalogueManagerTest.php
+++ Tests/Unit/Catalogue/CatalogueManagerTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Translation\Bundle\Tests\Unit\Catalogue;
 
 use PHPUnit\Framework\TestCase;
@@ -7,13 +16,16 @@ use Symfony\Component\Translation\MessageCatalogue;
 use Translation\Bundle\Catalogue\CatalogueManager;
 use Translation\Bundle\Model\CatalogueMessage;
 
+/**
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
 class CatalogueManagerTest extends TestCase
 {
     public function testGetMessages()
     {
         $manager = new CatalogueManager();
-        $catA = new MessageCatalogue('en', ['a' => 'aTrans', 'b'=>'bTrans']);
-        $catB = new MessageCatalogue('fr', ['a' => 'aTransFr', 'c'=>'cTransFr']);
+        $catA = new MessageCatalogue('en', ['a' => 'aTrans', 'b' => 'bTrans']);
+        $catB = new MessageCatalogue('fr', ['a' => 'aTransFr', 'c' => 'cTransFr']);
         $manager->load([$catA, $catB]);
         $messages = $manager->getMessages('en', 'message');
 
@@ -23,10 +35,10 @@ class CatalogueManagerTest extends TestCase
     public function testFinMessagesNoMetadata()
     {
         $manager = new CatalogueManager();
-        $catA = new MessageCatalogue('en', ['a' => 'aTrans', 'b'=>'bTrans']);
-        $catB = new MessageCatalogue('fr', ['a' => 'aTransFr', 'c'=>'cTransFr']);
+        $catA = new MessageCatalogue('en', ['a' => 'aTrans', 'b' => 'bTrans']);
+        $catB = new MessageCatalogue('fr', ['a' => 'aTransFr', 'c' => 'cTransFr']);
         $manager->load([$catA, $catB]);
-        $messages = $manager->findMessages(['locale'=>'en']);
+        $messages = $manager->findMessages(['locale' => 'en']);
 
         $this->assertCount(2, $messages);
     }
@@ -34,36 +46,36 @@ class CatalogueManagerTest extends TestCase
     public function testFinMessages()
     {
         $manager = new CatalogueManager();
-        $catA = new MessageCatalogue('en', ['a' => 'aTrans', 'b'=>'bTrans', 'c'=>'cTrans', 'd'=>'dTrans']);
-        $catA->setMetadata('a', ['notes'=>['category'=>'state', 'content'=>'new']]);
-        $catA->setMetadata('b', ['notes'=>['category'=>'state', 'content'=>'obsolete']]);
-        $catA->setMetadata('d', ['notes'=>['category'=>'approved', 'content'=>'true']]);
+        $catA = new MessageCatalogue('en', ['a' => 'aTrans', 'b' => 'bTrans', 'c' => 'cTrans', 'd' => 'dTrans']);
+        $catA->setMetadata('a', ['notes' => ['category' => 'state', 'content' => 'new']]);
+        $catA->setMetadata('b', ['notes' => ['category' => 'state', 'content' => 'obsolete']]);
+        $catA->setMetadata('d', ['notes' => ['category' => 'approved', 'content' => 'true']]);
 
-        $catB = new MessageCatalogue('fr', ['a' => 'aTransFr', 'c'=>'cTransFr', 'e'=>'eTransFr']);
-        $catA->setMetadata('c', ['notes'=>['category'=>'approved', 'content'=>'true']]);
-        $catA->setMetadata('e', ['notes'=>['category'=>'approved', 'content'=>'true']]);
+        $catB = new MessageCatalogue('fr', ['a' => 'aTransFr', 'c' => 'cTransFr', 'e' => 'eTransFr']);
+        $catA->setMetadata('c', ['notes' => ['category' => 'approved', 'content' => 'true']]);
+        $catA->setMetadata('e', ['notes' => ['category' => 'approved', 'content' => 'true']]);
 
         $manager->load([$catA, $catB]);
 
         // Only one approved en message
-        $messages = $manager->findMessages(['locale'=>'en', 'isApproved'=>true]);
+        $messages = $manager->findMessages(['locale' => 'en', 'isApproved' => true]);
         $this->assertCount(1, $messages);
         $this->assertEquals('d', $messages[0]->getKey());
 
-        $messages = $manager->findMessages(['isApproved'=>true]);
+        $messages = $manager->findMessages(['isApproved' => true]);
         $this->assertCount(3, $messages);
-        $keys = array_map(function(CatalogueMessage $message) {
+        $keys = array_map(function (CatalogueMessage $message) {
             return $message->getKey();
         }, $messages);
         $this->assertContains('c', $keys);
         $this->assertContains('d', $keys);
         $this->assertContains('e', $keys);
 
-        $messages = $manager->findMessages(['isNew'=>true]);
+        $messages = $manager->findMessages(['isNew' => true]);
         $this->assertCount(1, $messages);
         $this->assertEquals('a', $messages[0]->getKey());
 
-        $messages = $manager->findMessages([ 'isObsolete'=>true]);
+        $messages = $manager->findMessages(['isObsolete' => true]);
         $this->assertCount(1, $messages);
         $this->assertEquals('b', $messages[0]->getKey());
     }
