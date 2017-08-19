90599f1f619269802eec38093d798bcfa3ea3769
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-08-19 21:21:08 +0200

    Started with cataloguemanager test

---
+++ Tests/Unit/Catalogue/CatalogueManagerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Translation\Bundle\Tests\Unit\Catalogue;
+
+use PHPUnit\Framework\TestCase;
+use Translation\Bundle\Catalogue\CatalogueManager;
+
+class CatalogueManagerTest extends TestCase
+{
+    public function testFindMessages()
+    {
+        $manager = new CatalogueManager();
+    }
+}
