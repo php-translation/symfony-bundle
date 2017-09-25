862ff3bda83df9cbf09283fc36ea505a2227e0be
Damien ALEXANDRE <mobman02@gmail.com>
2017-09-25 10:42:39 +0200

    Add a --cache option on translation:download to clear the cache

--- Command/DownloadCommand.php
+++ Command/DownloadCommand.php
@@ -13,9 +13,11 @@ namespace Translation\Bundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Translation\Bundle\Service\StorageService;
+use Translation\Bundle\Model\Configuration;
 
 /**
  * @author Tobias Nyholm <tobias.nyholm@gmail.com>
@@ -27,15 +29,57 @@ class DownloadCommand extends ContainerAwareCommand
         $this
             ->setName('translation:download')
             ->setDescription('Replace local messages with messages from remote')
-            ->addArgument('configuration', InputArgument::OPTIONAL, 'The configuration to use', 'default');
+            ->addArgument('configuration', InputArgument::OPTIONAL, 'The configuration to use', 'default')
+            ->addOption('cache', null, InputOption::VALUE_NONE, 'Clear the cache if the translations have changed')
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $container = $this->getContainer();
         $configName = $input->getArgument('configuration');
+
         /** @var StorageService $storage */
         $storage = $container->get('php_translation.storage.'.$configName);
-        $storage->download();
+        /** @var Configuration $configuration */
+        $configuration = $this->getContainer()->get('php_translation.configuration.'.$configName);
+
+        if ($input->getOption('cache')) {
+            $translationsDirectory = $configuration->getOutputDir();
+            $md5BeforeDownload = $this->hashDirectory($translationsDirectory);
+            $storage->download();
+            $md5AfterDownload = $this->hashDirectory($translationsDirectory);
+
+            if ($md5BeforeDownload !== $md5AfterDownload) {
+                $command = $this->getApplication()->find('cache:clear');
+                $command->run(new ArrayInput(['--no-warmup' => true]), $output);
+            }
+        } else {
+            $storage->download();
+        }
+    }
+
+    private function hashDirectory($directory)
+    {
+        if (!is_dir($directory)) {
+            return false;
+        }
+
+        $files = [];
+        $dir = dir($directory);
+
+        while (false !== ($file = $dir->read())) {
+            if ($file !== '.' and $file !== '..') {
+                if (is_dir($directory.'/'.$file)) {
+                    $files[] = $this->hashDirectory($directory.'/'.$file);
+                } else {
+                    $files[] = md5_file($directory.'/'.$file);
+                }
+            }
+        }
+
+        $dir->close();
+
+        return md5(implode('', $files));
     }
 }
