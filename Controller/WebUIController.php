4509bfea6b08865ba0e7cf3ce7dbab837c6bf13f
bocharsky-bw <bocharsky.bw@gmail.com>
2019-06-20 00:55:35 +0300

    Remove unused namespace

--- Controller/WebUIController.php
+++ Controller/WebUIController.php
@@ -15,7 +15,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\Intl\Intl;
 use Symfony\Component\Intl\Locales;
 use Symfony\Component\Translation\MessageCatalogue;
 use Translation\Bundle\Exception\MessageValidationException;
