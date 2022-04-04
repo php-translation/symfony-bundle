1c0519ef8f3224cdf168acd8c8707a0705156629
Basil Baumgartner <32196755+seizan8@users.noreply.github.com>
2022-04-04 14:26:18 +0200

    fix: used finder->exclude instead of notPath for excludedDirs (#474)
    * fix: used finder->exclude instead of notPath for excludedDirs
    
    * docu: updated php comment in SymfonyProfilerController
    
    Co-authored-by: Basil Baumgartner <basil.baumgartner@ongoing.ch>

--- Command/ExtractCommand.php
+++ Command/ExtractCommand.php
@@ -143,7 +143,7 @@ class ExtractCommand extends Command
         $finder->in($config->getDirs());
 
         foreach ($config->getExcludedDirs() as $exclude) {
-            $finder->notPath($exclude);
+            $finder->exclude($exclude);
         }
 
         foreach ($config->getExcludedNames() as $exclude) {
